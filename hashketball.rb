6c94a6117e5cda6ff1f0822768540655096ca286
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-04 20:12:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -213,10 +213,10 @@ def big_shoe_rebounds
       if players[:shoe] > biggest_shoe
         biggest_shoe = players[:shoe]
         rebounds = players[:rebounds]
-        return rebounds
       end   
     end   
-  end     
+  end 
+  return rebounds
 end 
 
 
